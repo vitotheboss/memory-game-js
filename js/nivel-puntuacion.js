4d5030c3c4868f0823b56d63519ec12d82b3ee5d
Victor <vms@loop-interactiva.com>
2025-02-19 11:39:13 +0100

    clean —> scoring functions

--- js/nivel-puntuacion.js
+++ js/nivel-puntuacion.js
@@ -1,25 +1,3 @@
-// function puntuar() {
-//     const tiempoTotal = nivel[nivelActual].tiempo;
-//     const tiempoResto = cronometro.numSeg + (cronometro.numMin * 60);
-//     const totalCartas = nivel[nivelActual].cartas.length;
-//     const puntoMov = tiempoResto >= tiempoTotal/2 ? puntoBonus : puntoNormal;
-//     const puntosMax = totalCartas * puntoMov;
-//     // console.log('Tiempo Total:' ,tiempoTotal, 'Resto de Tiempo:' ,tiempoResto, ' punto:', puntoMov, 'Resto de Movimientos:' , );
-//     return Math.round((((movimientoMax()-movimientos)/movimientoMax())*puntosMax) + ((tiempoResto/tiempoTotal)*puntosMax)); 
-// }
-
-// function escribePuntos(nodo,puntos) {
-//     document.querySelector(nodo).innerHTML = `<span>${puntos}</span><i>puntos</i>`;
-// }
-
-// function almacenaPuntos() {
-//     if (!puntuacionAlmacena) return;
-//     let puntuacionNueva = puntuacion + puntuar();
-//     puntuacion = puntuacionNueva;
-//     puntuacionAlmacena = false;
-//     console.log(puntuacion);
-// }
-
 const puntos = {
     nivel: 0,
     acumulado: 0,
@@ -45,15 +23,13 @@ const puntos = {
     acumulaSuma() {
         if (!this.almacena) return;
         this.acumulado += this.nivel;
-
         this.cerrar();
         console.log(this.acumulado);
     },
-    
+
     acumulaResta() {
         if (!this.almacena) return;
         this.acumulado -= this.nivel;
-
         this.cerrar();
         console.log(this.acumulado);
     },
